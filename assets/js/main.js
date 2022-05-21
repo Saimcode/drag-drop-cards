f2e0efc7ce9d5ecfe59d339489ac97600e5cc83a
Saim <86623899+Saimcode@users.noreply.github.com>
2022-05-22 00:41:33 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*===== DRAG and DROP =====*/
+const dropItems = document.getElementById('drop-items');
+
+new Sortable(dropItems, {
+    animation: 200,
+    chosenClass: "sortable-chosen",
+    dragClass: "sortable-drag",
+    store: {
+    	set: (sortable) =>{
+    		const order = sortable.toArray()
+    		localStorage.setItem(sortable.options.group.name, order.join('|'))
+    	},
+
+    	get: (sortable) =>{
+    		const order = localStorage.getItem(sortable.options.group.name)
+    		return order ? order.split('|') : []
+    	}
+    }
+});
